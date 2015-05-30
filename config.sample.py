c76e75aab1c2f8bc4265626bee667e74c11c4fd3
Nicholas Whyte <nick@nickwhyte.com>
2015-05-31 03:46:57 +1000

    Config sample

--- config.sample.py
+++ config.sample.py
@@ -2,7 +2,6 @@ from Item import Item
 
 MAX_FOLDER_DL_SIZE_BYTES = 2 * 1000 * 1000 * 1000 # GB to Bytes
 ROOT_PATHS = [
-    Item('/', 'Macintosh HD'),
-    Item('/Volumes/iData/', 'iData')
+    Item('/', 'Root'),
 ]
 IGNORE_FILES = r'^\..*$'
